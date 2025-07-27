400c1965fc2884ce0a374f8ddb45161cea3bb3f3
conao3 <conao3@gmail.com>
2025-07-28 01:19:02 +0900

    add commands router

--- src/skkjure/core.clj
+++ src/skkjure/core.clj
@@ -1,7 +1,33 @@
 (ns skkjure.core)
 
+(declare commands)
+
 (defn add [a b]
   (+ a b))
 
-(defn -main [& _args]
-  (println "Hello, world"))
+(defn eprintln [& args]
+  (binding [*out* *err*]
+    (apply println args)))
+
+(defn cmd-serve
+  "Serve skkjure server"
+  [& args]
+  (eprintln args))
+
+(defn cmd-help
+  "Help"
+  [& _args]
+  (eprintln "Available commands:")
+  (doseq [[k v] commands]
+    (-> (format "  %s - %s" (name k) (:doc (meta v)))
+        eprintln)))
+
+(def commands {:serve #'cmd-serve
+               :help #'cmd-help})
+
+(defn -main [& args]
+  (if-let [command (get commands (keyword (first args)))]
+    (apply command args)
+    (do (when (first args)
+          (eprintln (format "`%s' is not a skkjure command." (first args))))
+        (apply cmd-help args))))
