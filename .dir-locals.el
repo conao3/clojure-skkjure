1898cc41eb48447f7117f8b36194ae019abd825d
conao3 <conao3@gmail.com>
2025-07-29 07:31:30 +0900

    add :repl alias

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((cider-clojure-cli-aliases . ":dev:build")
+((nil . ((cider-clojure-cli-aliases . ":dev:build:repl")
          (cider-ns-refresh-before-fn . "com.stuartsierra.component.repl/stop")
          (cider-ns-refresh-after-fn  . "com.stuartsierra.component.repl/start"))))
